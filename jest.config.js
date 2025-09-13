07e1154ba10bad8671d2cd0731bcfbdceaf90e14
skrastins58-source <skrastins58@gmail.com>
2025-09-14 01:14:28 +0300

    feat: complete CI/CD pipeline setup

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/src/**/*.test.js'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json-summary', 'text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
+};
